4ea1ce10e95d06f664a670fee74bc7850f69a87b
Hanin Mustafa <haninmustafa@Hanins-MacBook-Air.local>
2016-11-28 22:33:59 -0800

    styling footer

--- themes/mountain_health/footer.php
+++ themes/mountain_health/footer.php
@@ -10,24 +10,25 @@
 	<!-- #content -->
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-				<img src="<?php echo get_template_directory_uri(). "/images/footer_mountain.png " ?>" alt="Mountain Logo" ?>
+			<img class="mountain-logo-footer" src="<?php echo get_template_directory_uri(). "/images/footer_mountain.png " ?>" alt="Mountain Logo"
+							?>
 			<div class="newsletter">
-				<h2>Join our newsletter</h2>
+				<h2 class="footer-title">Join our newsletter</h2>
 				<p>Get the latest news about Naturopathy and more.</p>
 				<form class="footer-form">
-					<input type="email" name="email" value="Your Email" id="the-email" placeholder="yourname@example.com">
+					<input type="email" name="email" value="yourname@example.com" id="the-email">
 					<input type="submit" name="submit" value="Subscribe">
 				</form>
 			</div>
-			<div>
+			<div class="footer-info">
 				<div>
-					<h2>Contact Us</h2>
+					<h2 class="footer-title">Contact Us</h2>
 					<p>Mountain Health
 						<br> Phone: (727)362-2429 info@mountainhealth.com
 						<br> 202 Murtle Road Clearwater, BC V0E 1N1</p>
 				</div>
 				<div>
-					<h2>Hours</h2>
+					<h2 class="footer-title">Hours</h2>
 					<p>Sunday <br> Monday
 						<br> Tuesday
 						<br> Wednesday
@@ -47,13 +48,13 @@
 					</p>
 					<img src="<?php echo get_template_directory_uri(). "/images/Social/Linkedin-Dark.png " ?>" alt="Mountain Logo" ?>
 				</div>
-				<div>
-					<h2>Location</h2>
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3147.703308483417!2d-123.0350735712222!3d49.306316813241324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x97c6446531682d55!2sMountain+Health+and+Performance!5e0!3m2!1sen!2sca!4v1480396461276"
-									width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-				</div>
 			</div>
 			<div>
+				<h2 class="footer-title">Location</h2>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3147.703308483417!2d-123.0350735712222!3d49.306316813241324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x97c6446531682d55!2sMountain+Health+and+Performance!5e0!3m2!1sen!2sca!4v1480396461276"
+								width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+			<div class="copy-footer">
 				<p>Copyright &copy; 2016 Mountain Health. All rights reserved.</p>
 			</div>
 		</div>
