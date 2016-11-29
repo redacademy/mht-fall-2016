056dd48b19fd90a600454369ec96bbc5986ed14d
shamu92 <altshteyn@gmail.com>
2016-11-29 14:05:47 -0800

    structure of resources page

---
+++ themes/mountain_health/resources.php
@@ -0,0 +1,49 @@
+<?php /* Template Name: Resources Page */
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+<div class="resources-hero">	
+	<h1>Resources</h1>
+</div>
+<p>
+ We are always here to help, but sometimes you have
+ questions in between appointments.
+</p>
+<p>
+Knowledge is key in taking control of your health, so 
+we provide you with additional articles from Dr. Chay, 
+recipes, and the latests news from Mountain Health.
+</p>
+<div class="news-events">
+	<h2>News + Events</h2>
+	<p>
+	Learn about what’s new at Mountain Health and 
+	discover upcoming events.
+	</p>
+	<a href="#">Read More</a></div>
+<div class="articles=research">
+	<h2>Articles + Research</h2>
+	<p>
+	Read our articles from Dr. Chay and find more 
+	information about health and naturopathy.
+	</p>
+	<a href="#">Read More</a>
+</div>
+<div class="recipes-nutrition">
+	<h2>Recipes + Nutrition</h2>
+	<p>
+	Build your collection of healthy recipes and 
+	nutritional tips from Dr. Chay
+	</p>
+	<a href="#">Read More</a>
+</div>
+<a href="#">
+	<h2>Learn more about 
+	conditions and evidence-based treatments >>
+</a>
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
