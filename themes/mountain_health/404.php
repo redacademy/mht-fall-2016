549a054bb21e72114bfe2eeee7e3fd370c396d68
Kadyn Neale <kneale95@hotmail.ca>
2016-11-29 14:44:06 -0800

    404 html created

--- themes/mountain_health/404.php
+++ themes/mountain_health/404.php
@@ -11,44 +11,23 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php echo esc_html( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?' ); ?></p>
-
-					<?php get_search_form(); ?>
-
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( red_starter_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php echo esc_html( 'Most Used Categories' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-						$archive_content = '<p>' . sprintf( esc_html( 'Try looking in the monthly archives. %1$s' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
+		<section class="page-hero">
+			<h1>404</h1>
+			<p>We're sorry!</p>
+			<p>Looks like we sent you to the wrong place.</p>
+		</section>
+		<section class="404-content">
+			<div class="404-div">
+				<img src="#" alt="apple">
+				<p>Would a snack help?</p>
+				<a href="#" class="green-button">View Recipies</a>
+			</div>
+			<div class="404-div">
+				<img src="#" alt="text-bubble">
+				<p>Send us a message here.</p>
+				<a href="#" class="green-button">Contact Us</a>
+			</div>
+		</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
