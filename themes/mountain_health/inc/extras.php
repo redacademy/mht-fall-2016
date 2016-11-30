09a7c4a7835d92ab1ae65b162ec42fab1f9b2d3d
Hanin Mustafa <haninmustafa@Hanins-MacBook-Air.local>
2016-11-30 13:42:22 -0800

    adding custom field to the lab testing page

--- themes/mountain_health/inc/extras.php
+++ themes/mountain_health/inc/extras.php
@@ -22,28 +22,3 @@ function red_starter_body_classes( $classes ) {
 add_filter( 'body_class', 'red_starter_body_classes' );
 
 
-//showing the custom image for the appointment page 
-function mht_appointment_image_styles() {
-        //checking for page template
-        if( ! is_page_template('appointments.php')){
-            return;
-        }
-            $appointment_image = CFS()->get( 'appointment_img' );
-            //checking to see if variable is empty or not.
-            if( ! $appointment_image){
-                return;
-            } 
-            
-            // $custom_css = "
-            //         .appointment_img{
-            //             width: 100vw;
-            //             height: 100vh;
-            //             background: 
-            //                 linear-gradient( to bottom, rgba(0,0,0, 0.5), rgba(0,0,0, 0.5)),
-            //                 url({$appointment_image});
-            //             background-size: cover;
-            //         }";
-            // wp_add_inline_style( 'custom-style', $custom_css );
-
-}
-add_action( 'wp_enqueue_scripts', 'mht_appointment_image_styles' );
