b527b07ca70223262d81e6819f5a0d87b9a32a89
Hanin Mustafa <haninmustafa@Hanins-MacBook-Air.local>
2016-11-30 10:22:50 -0800

    adding custom field loop for appointments page

--- themes/mountain_health/appointments.php
+++ themes/mountain_health/appointments.php
@@ -6,18 +6,26 @@
  */
 
 get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <h1>Our appointments</h1>
+            <a href="#">Home Page /</a><a href="#"> What We Do /</a><a href="#"> Appointments</a>
+            <p>At Mountain Health we like to keep our wait times short and our visits long enough to answer all your questions.
+                Your time is valuble and you should get the most out of every visit. We are here to listen to all your concerns
+                and find how we can best help you.</p>
+            <?php $fields = CFS()->get( 'appointment' );
+                   foreach ( $fields as $field ) {
+                   echo $field['appointment_img'];
+                   echo $field['appointment_title'];
+                    echo $field['appointment_text'];
+                    echo $field['appointment_price'];
+                    echo $field['appointment_duration'];
+} ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'template-parts/content', 'page' ); ?>
+            <?php endwhile; // End of the loop. ?>
+        </main>
+        <!-- #main -->
+    </div>
+    <!-- #primary -->
+    <?php get_footer(); ?>
