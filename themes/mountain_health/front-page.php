15f031bef55e25ac793956c25455bd25b13e1474
Kadyn Neale <kneale95@hotmail.ca>
2016-11-30 09:02:50 -0800

    front page placeholder syled

--- themes/mountain_health/front-page.php
+++ themes/mountain_health/front-page.php
@@ -11,56 +11,70 @@ get_header(); ?>
     <main id="main" class="site-main" role="main">
 
 <section class="hero-banner"> <!--Front page banner-->
-    <h2 class-"hero-header">Mountain Health</h2>
-    <p>To empower every person to have a fulfilling healthy life and feel connected
-     to their community and their environment.</p>
-    <p>To change the outcome of chronic disease.</p>
-    <p>To remain curious about each individual and improve their quality of life with
-    dedication and passion</p>
-    <a href="#">Meet Dr.Chay > </a>
-    <h2> What is Naturopathic Medicine? </h2>
+    <h2 class="hero-header">Mountain<br>
+    Health</h2>
+    <div class="header-text">
+        <p>To empower every person to have a fulfilling healthy life and feel connected
+        to their community and their environment.</p>
+        <p>To change the outcome of chronic disease.</p>
+        <p>To remain curious about each individual and improve their quality of life with
+        dedication and passion</p>
+        <a href="#">Meet Dr.Chay > </a>
+    </div>
+    <h2 class="what-is-hero"> What is Naturopathic<br>
+    Medicine? </h2>
+    <p>Naturopathic medicine combines the best of<br>
+    traditional treatments with evidence-based<br>
+    medicine.</p>
 </section><!--End of hero-banner-->
 
-<section class="Booking"><!--Booking for front page-->
+<section class="booking"><!--Booking for front page-->
     <img src="#">
-    <p>Naturopathic medicine offers individualized, natural healthcare.</p>
+    <p>Naturopathic medicine offers<br>
+    individualized, natural healthcare.</p>
     <button class="green-button">Book an appointment</button>  
 </section>
 
 <section class="benifits">
-    <h2>Benifits of Naturopathic Medical Treatment</h2>
-    <img src="#">
-    <ul class="benifits-list">
-        <li>Increased energy</li>
-        <li>Weight loss</li>
-        <li>Decreased pain</li>
-        <li>Decreased symptoms</li>
-        <li>Less side effects from treatment</li>
-        <li>Better sleep</li>
-        <li>Increased libido</li>
-        <li>Optimal health + Lifestyle</li>
-    </ul>
+    <h2>Benifits of Naturopathic<br>
+    Medical Treatment</h2>
+    <div class="benifits-content">
+        <img src="#">
+        <ul class="benifits-list">
+            <li>Increased energy</li>
+            <li>Weight loss</li>
+            <li>Decreased pain</li>
+            <li>Decreased symptoms</li>
+            <li>Less side effects from treatment</li>
+            <li>Better sleep</li>
+            <li>Increased libido</li>
+            <li>Optimal health + Lifestyle</li>
+        </ul>
+    </div>
 </section>
 
 <section class="compare-to-traditional">
     <h2>Compared to traditional health care</h2>
-    <div class="better-appointments">
-        <img src="#">
+    <div class="compare-content">
+        <img src="#" class="clock" alt="clock">
         <h3>Better appointments</h3>
         <ul>
             <li>Minimal wait times</li>
-            <li>Longer visits</li>
-            <li>Time spent listening to all of your concerns</li>
+            <li>Longer visits*</li>
+            <li>Time spent listening to all of<br>
+            your concerns</li>
         </ul>
         <button class="green-button">Book an appointment</button>
-        <p>*all inital visits are limited to one hour to avoid taking too much time out of your day</p>
+        <p class="note">*all inital visits are limited to one hour,<br>
+        to avoid taking too much time out of your day</p>
     </div>
-    <div class="more-control">
-        <img src="#" alt="person silouette">
+    <div class="compare-content">
+        <img src="#" alt="person silouette" class="silouette">
         <h3>More control</h3>
         <ul>
             <li>A large range of treatment options</li>
-            <li>Explanations of treatment and your concerns</li>
+            <li>Explanations of treatment and<br>
+            your concerns</li>
             <li>individualized treatment</li>
         </ul>
         <button class="green-button">View Treatments</button>
@@ -80,11 +94,11 @@ get_header(); ?>
 
 <section class="did-you-know">
     <h2>Did you know?</h2>
-    <p>1</p>
+    <p class="front-number">1</p>
     <p>Naturopathic physicians must attend the same pre-medical studies as MDs and, they must obtain a four year bachelour degree prior to applying to one of the seven accredited schools in North America</p>
-    <p>2</p>
+    <p class="front-number">2</p>
     <p>To obtain the title of Naturopathic Doctor (ND), the naturopathic physician attends four years of full-time post graduate studies.</p>
-    <p>3</p>
+    <p class="front-number">3</p>
     <p>Like MD students, students of a Naturopathic Medical Program cover physiology, pathoology, and basic sciences, in addition to studies on herbal medicine, vitamins and lifesyle.</p>
 </section>
 
