0a9011323b469c5d34928317a26f18b1455f1119
Hanin Mustafa <haninmustafa@Hanins-MacBook-Air.local>
2016-11-30 22:45:01 -0800

    adding show/hide button for news and events

--- themes/mountain_health/functions.php
+++ themes/mountain_health/functions.php
@@ -84,7 +84,9 @@ add_filter( 'stylesheet_uri', 'red_starter_minified_css', 10, 2 );
  */
 function red_starter_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
-
+	$script_url = get_template_directory_uri() . '/js/scripts.js';
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'mountain_health_theme', $script_url, array( 'jquery' ), false, true );
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
--- themes/mountain_health/newsevents.php
+++ themes/mountain_health/newsevents.php
@@ -4,20 +4,18 @@
  * Template Name: News & Events Page
  * @package Mountain_Heath_Theme
  */
-
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
+			<button class="news-section">News</button>
+            <button class="events-section">Events</button>
+            <div class="news-lists"> <p>test1</p> </div>
+            <div class="events-lists" style="display:none;"> <p>test2</p> </div>
+            
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
