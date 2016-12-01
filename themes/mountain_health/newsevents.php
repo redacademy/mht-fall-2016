e614d5475901330669bf05a04d8a6c1b5d45af86
Hanin Mustafa <haninmustafa@Hanins-MacBook-Air.local>
2016-11-30 23:13:45 -0800

    displaying events custom field after clicking

--- themes/mountain_health/newsevents.php
+++ themes/mountain_health/newsevents.php
@@ -12,7 +12,24 @@ get_header(); ?>
 			<button class="news-section">News</button>
             <button class="events-section">Events</button>
             <div class="news-lists"> <p>test1</p> </div>
-            <div class="events-lists" style="display:none;"> <p>test2</p> </div>
+            <div class="events-lists" style="display:none;"> 
+                
+            <?php $fields = CFS()->get( 'event'); ?>
+            <?php   foreach ( $fields as $field ) : ?>
+
+            <div class= "events_img">
+                <?php echo '<img src="' . wp_get_attachment_image_src($field['events_img'], 'medium') . '" />'; ?>
+            </div>
+            <img src=" <?php CFS()->get( 'events_img' ); ?>"/>
+            
+            <img src="<?php  echo $field['events_img']; ?>"/>
+                <h3> <?php   echo $field['event_title']; ?> </h3>
+               <p>  <?php   echo $field['event_text']; ?> </p>
+              <p>  <?php   echo $field['events_date']; ?> </p>
+               <p> <?php   echo $field['events_location']; ?> </p>
+                <?php endforeach; ?>
+
+                 </div>
             
 
 		</main><!-- #main -->
