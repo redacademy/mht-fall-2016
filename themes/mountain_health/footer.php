68c1ebda69697a3f510825e24327824d2ced5558
Hanin Mustafa <haninmustafa@Hanins-MacBook-Air.local>
2016-12-02 14:21:20 -0800

    making footer fully responsive

--- themes/mountain_health/footer.php
+++ themes/mountain_health/footer.php
@@ -22,38 +22,37 @@
 			<div class="footer-info">
 				<div class="footer_wrapper">
 					<h2 class="footer-title">Contact Us</h2>
-					<p>Mountain Health
+					<p><span class="thick-span">Mountain Health</span>
 						<br>1037 Cornwall Street
 						<br>New Westminster, BC V3M 1S1</p>
 					<div class="contact_icon_img">
-						<img src="<?php echo get_template_directory_uri(). "/images/Social/Facbook-Dark.png " ?>" alt="Mountain Logo" ?>
-						<a href="tel:XXX-XXX-XXXX">XXX-XXX-XXXX</a>
+						<img class="" src="<?php echo get_template_directory_uri()?>/MTH-Assets/logos/phone.png" alt="Phone logo">
+						<a class="link-footer" href="tel:XXX-XXX-XXXX">XXX-XXX-XXXX</a>
 					</div>
 					<div class="contact_icon_img">
-						<img src="<?php echo get_template_directory_uri(). "/images/Social/Facbook-Dark.png " ?>" alt="Mountain Logo" ?>
-						<a href="mailto:info@mountainhealth.com">info@mountainhealth.com</a>
+						<img class="" src="<?php echo get_template_directory_uri()?>/MTH-Assets/logos/email.png" alt="Email logo">
+						<a class="link-footer"href="mailto:info@mountainhealth.com">info@mountainhealth.com</a>
 					</div>
 				</div>
 				<div class="footer_wrapper">
 					<h2 class="footer-title">Find Us</h2>
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3147.703308483417!2d-123.0350735712222!3d49.306316813241324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x97c6446531682d55!2sMountain+Health+and+Performance!5e0!3m2!1sen!2sca!4v1480396461276"
-									width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2601.427445150521!2d-123.03500823466057!3d49.306188277026386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5486708c9df02d4b%3A0x97c6446531682d55!2sMountain+Health+and+Performance!5e0!3m2!1sen!2sca!4v1480714880107"
+							width="280" height="220" frameborder="0" style="border:0" allowfullscreen></iframe>
 				</div>
 			<div class="footer_wrapper">
-			<h2>Information</h2>
-			<p>Hours:</p>
-			<p><span class="thick-text-footer">Tues - Fri 9am - 5pm </span><br>
+			<h2 class="footer-title">Information</h2>
+			<p>Hours:<br>
+			<span class="thick-span">Tues - Fri 9am - 5pm </span><br>
 			Closed for lunch from 1pm - 2pm<br>
-
+			<br>
 			Exit Lorem ipsum dolor sit amet.
 			Located in Lorem ipsum dolor sit amet.
 			For wheelchair access, park in the back.<p>
 			</div>
-
+		</div>
 				<div class="copy-footer">
 					<p>Copyright &copy; 2016 Mountain Health. All rights reserved.</p>
 				</div>
-			</div>
 			<!-- .site-info -->
 	</footer>
 	<!-- #colophon -->
