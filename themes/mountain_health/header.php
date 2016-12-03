93654a9ece38efdb12ce4423b02598e915dc8fc5
shamu92 <altshteyn@gmail.com>
2016-12-03 15:37:51 -0800

    5px;

--- themes/mountain_health/header.php
+++ themes/mountain_health/header.php
@@ -21,6 +21,12 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
+				
+				<img class="MTH-logo" src="<?php echo get_template_directory_uri()?>/MTH-Assets/logos/logo-green.png" alt="MTH logo">
+				<a href="#" onclick="popup('popUpDiv')"><img src="<?php echo get_template_directory_uri()?>/MTH-Assets/icons/phone-blue.png" alt="MTH logo"></a>
+				<p>1-562-867-5309</p>
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?><i class="fa fa-bars" aria-hidden="true"></i></button>
+
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
@@ -28,27 +34,25 @@
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?><i class="fa fa-bars" aria-hidden="true"></i></button>
 					
 					<div class="mobile-menu">
+						<?php get_search_form(); ?>		
 							<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 						
 						<div class="search-and-patient-portal">
-							<?php get_search_form(); ?>						
-							<a href="tel:">1-562-867-5309</a>
+									
 						</div>
 					</div>	
 				</nav><!-- #site-navigation -->
-					<img class="MTH-logo" src="<?php echo get_template_directory_uri()?>/MTH-Assets/logo-green.png" alt="MTH logo">
-				<p>(727) 362-2429</p>
 				
 				<div id="blanket" style="display:none"></div>
 				<div id="popUpDiv" style="display:none">
 
 					<a href="#" onclick="popup('popUpDiv')" >Do Not Call!</a>
 					<a href="tel:1-562-867-5309">1-562-867-5309</a>
+
 					</div>	
-						<a href="#" onclick="popup('popUpDiv')"><i class="fa fa-phone-square fa-3x" aria-hidden="true"></i></a>
+						
 
 			</header><!-- #masthead -->
 
