d4a0c956699e4cc04b8b6c8b152c899a437063fa
shamu92 <altshteyn@gmail.com>
2016-12-04 00:12:35 -0800

    made and styled mobile search

--- themes/mountain_health/js/main.js
+++ themes/mountain_health/js/main.js
@@ -3,13 +3,20 @@
     $('.menu-toggle').on('click', function(event) {
         event.preventDefault();
         $('.mobile-menu').animate({ height: 'toggle' });
-        // $('#primary-menu').toggle("slide", { direction: "left" }, 5000);
+        // shows the navigation when hamburger is clicked
     });
+    // toggles hamburger when clicked and shows X on menu bar
     $('.x-logo').hide();
     $('.menu-toggle').on('click', function(event) {
         event.preventDefault();
         $('.hamburger').toggle();
         $('.x-logo').toggle();
+    });
+    $('.search-field-mobile').hide();
+    $(".search").on('click', function() {
+        $('header, #primary, .mobile-menu, footer').hide();
+        $('.search-field-mobile').show();
+
     });
     // news and events button to show/hide news/events
     $(".events-section").on('click', function() {
