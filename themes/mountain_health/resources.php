8761fa31baf38e3c2d160a1d65749bd83b2a0f11
shamu92 <altshteyn@gmail.com>
2016-12-07 11:53:05 -0800

    resources content updated

--- themes/mountain_health/resources.php
+++ themes/mountain_health/resources.php
@@ -5,37 +5,38 @@ get_header(); ?>
 	<main id="main" class="site-main" role="main">
 <div class="resources-hero secondary-hero">	
 	<h1>Resources</h1>
-	<p>We are always here to help, but sometimes you have questions in between appointments. Knowledge is</p>
+	<p>We are here to help, but sometimes you have questions in between appointments. We provide you with articles, recipes, and more.</p>
 </div>
 <section class="resources">
 	<div class="news-events">
-		<h2>News + Events</h2>
+		<h2>News & Events</h2>
 		<div class="resources-newspaper"></div>
 		<p>
-		Learn about what’s new at Mountain Health and 
-		discover upcoming events.
+		Learn about what’s new at Mountain Health and discover upcoming events.
 		</p>
 		<a href="#" >Read More</a>
+		<!--Button links to 3.1 News and events-->
 	</div>
 	
 	<div class="articles-research">
 		<h2>Articles + Research</h2>
 		<div class="resources-microscope"></div>
 		<p>
-		Read our articles from Dr. Chay and find more 
-		information about health and naturopathy.
+		Read our articles from Dr. Chay and find more information about health.
 		</p>
 		<a href="#">Read More</a>
+		<!--button links to 3.4 articles and research-->
 	</div>
 
 	<div class="recipes-nutrition">
 		<h2>Recipes + Nutrition</h2>
 		<div class="resources-apple"></div>
 		<p>
-		Build your collection of healthy recipes and 
-		nutritional tips from Dr. Chay
+		Healthy recipes and 
+		nutritional tips.
 		</p>
 		<a href="#">Read More</a>
+		<!--button links to 3.3 recipes and nutrition-->
 	</div>
 </section>
 	</main><!-- #main -->
