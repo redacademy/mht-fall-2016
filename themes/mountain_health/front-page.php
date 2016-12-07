9881cf06caaa9f3be435eac2bf51056f6c40c478
shamu92 <altshteyn@gmail.com>
2016-12-07 10:17:11 -0800

    changed footer and front page content

--- themes/mountain_health/front-page.php
+++ themes/mountain_health/front-page.php
@@ -14,8 +14,8 @@ get_header(); ?>
             <div class="parallax-scroll"></div>
                 <h2 class="hero-header">Mountain Health</h2>
             <div class="header-text">
-                <p>To empower every person to have a fulfilling healthy life and feel connected
-                    to their community and their environment.</p>
+                <p>Empowering every person to have a fulfilling, healthy life, 
+                    and feel connected to their community and the environment.</p>
             </div>
             <div class="keep-scrolling"></div>
         </section><!--End of hero-banner-->
@@ -24,10 +24,10 @@ get_header(); ?>
         <section class="booking"><!--Booking for front page-->
             <div></div>
             <h2 class="what-is-hero"> What is Naturopathic Medicine? </h2>
-                <p>Naturopathic medicine is a type of healthcare that offers individualized,
-                natural healthcare. It combines the best of traditional treatments with
-                evidence-based medicine. </p>
-            <a href="#" class="front-button what-is-button">Book an appointment</a>
+                <p>Naturopathy is a type of care that offers individualized, natural healthcare. 
+                    It combines the best of traditional treatments with evidence-based medicine. </p>
+            <a href="#" class="front-button what-is-button">Read More</a> 
+            <!--button above links to articles and resources 3.4-->
         </section>
 
         <section class="benifits">
@@ -41,12 +41,12 @@ get_header(); ?>
                 <li><span class="list-span">Less side effects from treatment</span></li>
                 <li><span class="list-span">Better sleep</span></li>
                 <li><span class="list-span">Increased libido</span></li>
-                <li><span class="list-span">Optimal health + Lifestyle</span></li>
+                <li><span class="list-span">Optimal health & Lifestyle</span></li>
             </ul>
         </section>
 
         <section class="compare-to-traditional">
-            <h2>Naturopathy compared to traditional health care</h2>
+            <h2>Benifits of Working With Us</h2>
             <div class="compare-flex">
                 <div class="compare-content">
                     <div class="clock"></div>
@@ -56,9 +56,10 @@ get_header(); ?>
                         <li>Minimal wait times</li>
                         <li>Longer visits*</li>
                         <li>Time spent listening to all of<br>
-                        your concerns</li>
+                        your health concerns</li>
                     </ul>
-                    <a href="#" class="front-button what-is-button">Book an appointment</a>
+                    <a href="#" class="front-button what-is-button">See Our Appointments</a>
+                                    <!--button above links to appointments 2.4-->
                     <p class="note">*all inital visits are limited to one hour,<br>
                     to avoid taking too much time out of your day</p>
                 </div>
@@ -68,11 +69,13 @@ get_header(); ?>
                     <div class="underline-strip"></div>
                     <ul class="silouette-ul">
                         <li><span class="list-span">A large range of treatment options</span></li>
-                        <li><span class="list-span">Explanations of treatment and
-                        your concerns</span></li>
+                        <li><span class="list-span">Explanations of treatments and
+                        your health concerns</span></li>
                         <li><span class="list-span">Individualized treatment</span></li>
                     </ul>
-                    <a href="#" class="front-button what-is-button">See our treatments</a>
+                    <a href="#" class="front-button what-is-button">See Our Treatments</a>
+                    <p>*All initial visits are one hour, to avoid taking too much time out of your day</p>
+                                                <!--button above links to treatments 2.2-->
                 </div>
             </div>  
         </section>
@@ -80,58 +83,73 @@ get_header(); ?>
         <section class="what-is">
             <div class="what-is-header">
                 <div class="white-box">
-                    <p>Naturopathic doctors use both
-                    treatments and testing that are
-                    evidence based and also those that have been proven to work
-                    traditionally through clinical experience.</p>
-                    <button class="front-button flex-button">See our treatments</button>
+                    <p>Naturopathic doctors use both treatments and testing that are evidence based, 
+                        and also those that have been proven to work traditionally through clinical experience.</p>
+                    <button class="front-button flex-button">See More About Us</button>
+                                            <!--Button above links to about 1.0-->
                 </div>
             </div>
             <div class="main-carousel" data-flickity='{ "cellAlign": "center", "contain": true, "autoPlay": 3000, "prevNextButtons": false, "pageDots": false}'>
                 <div class="carousel-cell front-page-slider">
-                    <h2>What is a Naturopathic physician?</h2>
-                    <p>A primary care provider trained in both natural and pharmaceutical therapies. Naturopathic physicians can prescribe</p>
-                    <a href="#" class="front-button">Read More</a>
+                    <h2>What is a Naturopathic Physician?</h2>
+                    <p>"A primary care provider trained in both natural and pharmaceutical therapies. 
+                        Naturopathic physicians can prescribe drugs, herbs, vitamins and exercise therapy.”
+                    </p>
+                    <a href="#" class="front-button">Our Lab Testing</a>
+                    <!--button links to Lab testing 2.3-->
+                </div>
+
+                <div class="carousel-cell front-page-slider">
+                    <h2>What is a Naturopathic Physician?</h2>
+                    <p>“As a primary care provider, they can also perform minor surgery, IV therapy and take 
+                        blood samples.”
+                    </p>
+                    <a href="#" class="front-button">Our Lab Testing</a>
+                    <!--button links to Lab testing 2.3-->
                 </div>
 
                 <div class="carousel-cell front-page-slider">
                     <h2>What is a Naturopathic physician?</h2>
-                    <p>A primary care provider trained in both natural and pharmaceutical therapies. Naturopathic physicians can prescribe</p>
-                    <a href="#" class="front-button">Read More</a>
-                </div>
-
-                <div class="carousel-cell front-page-slider">
-                    <h2>What is a Naturopathic physician?</h2>
-                    <p>A primary care provider trained in both natural and pharmaceutical therapies. Naturopathic physicians can prescribe</p>
-                    <a href="#" class="front-button">Read More</a>
+                    <p>“Naturopathic physicians have access to life labs as well as various other private, 
+                        innovative lab companies.”
+                    </p>
+                    <a href="#" class="front-button">Our Lab Testing</a>
+                    <!--button links to Lab testing 2.3-->
                 </div>
             </div>
         </section>
         <section class="did-you-know">
             <div class="did-you-know-pic"></div>
-            <h2>Did you know?</h2>
+            <h2>Did You Know?</h2>
             <div class="number-circle">
                 <p class="front-number">1</p>
             </div>
             <div class="did-you-know-bg did-you-know-bg-1">
-                <p>Naturopathic physicians must attend the same pre-medical studies as MDs and, they must obtain a four year bachelour degree prior to applying to one of the seven accredited schools in North America</p>
+                <p>Naturopathic physicians must attend the same pre-medical studies as MDs, 
+                    and they must obtain a four year degree prior to applying to one of the 
+                    seven accredited schools in North America.
+                </p>
             </div>
             <div class="number-circle">
                 <p class="front-number">2</p>
             </div>
             <div class="did-you-know-bg did-you-know-bg-2">
-                <p>To obtain the title of Naturopathic Doctor (ND), the naturopathic physician attends four years of full-time post graduate studies.</p>
+                <p>To obtain the title of Naturopathic Doctor (ND), the naturopathic physician 
+                    attends four years of full-time post graduate studies.</p>
             </div>
             <div class="number-circle">
                 <p class="front-number">3</p>
             </div>
             <div class="did-you-know-bg did-you-know-bg-3">
-                <p>Students of a Naturopathic Medical program cover the same physiology,pathology, and basic sciences as MDs, in addition to studies on herbal medicine, vitamins and lifestyle.</p>
+                <p>Students of a Naturopathic Medical program cover the same physiology, pathology, 
+                    and basic sciences as MDs, in addition to studies on herbal medicine, vitamins and lifestyle.
+                </p>
             </div>
         </section>
         <div class="how-can">
             <h4 class="how-can-we-help-you">Do you want to know how we can help you?</h4>
             <a href="#" class="front-button flex-button">Book an appointment</a>
+                                <!--button above links to book appointment 5.0-->
         </div>
     </main><!-- #main -->
 </div><!-- #primary -->
