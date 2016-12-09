7a6caacc872dee51f18171ee83d72abd5cae808c
Kadyn Neale <kneale95@hotmail.ca>
2016-12-09 12:05:50 -0800

    fixed unclosed tag breaking game

--- themes/mountain_health/what-we-do.php
+++ themes/mountain_health/what-we-do.php
@@ -11,7 +11,7 @@
         </div>
         <section class="nav-links article-nav-links-archive bread-crumb-general what-we-do-bread">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>"> Home page &gt; </a>
-				<a href="<?php echo get_page_link(23); ?>">
+				<a href="<?php echo get_page_link(23); ?>"></a>
 				<p> &nbsp;<?php wp_title( '' ); ?></p>
 		</section>
         <section class="treatments-testing-appointments">
