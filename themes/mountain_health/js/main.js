00659426d1de874a7c030f316ea48b69042ab64e
Hanin Mustafa <haninmustafa@Hanins-MacBook-Air.local>
2016-12-11 02:55:25 -0800

    adding graphical countdown and redirect after 5seconds in redirect page

--- themes/mountain_health/js/main.js
+++ themes/mountain_health/js/main.js
@@ -131,4 +131,14 @@
         $('.search-state-dt').animate({ height: 'toggle' });
     });
 
+    //adding counter to the redirect page
+    function countdown() {
+    var i = document.getElementById('counter');
+    if (parseInt(i.innerHTML)<=0) {
+        location.href = 'login.php';
+    }
+    i.innerHTML = parseInt(i.innerHTML)-1;
+}
+setInterval(function(){ countdown(); },1000);
+
 })(jQuery);
