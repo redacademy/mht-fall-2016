f6bb8084cd7c9b96b96a2c48e279e8bea7ff1463
Hanin Mustafa <haninmustafa@Hanins-MacBook-Air.local>
2016-12-13 02:32:17 -0800

    dploying

--- themes/mountain_health/functions.php
+++ themes/mountain_health/functions.php
@@ -205,23 +205,3 @@ function excerpt($limit) {
       $content = str_replace(']]>', ']]&gt;', $content);
       return $content;
     }
-
-// add arrows to menu parent 
-function oenology_add_menu_parent_class( $items ) {
-
- $parents = array();
- foreach ( $items as $item ) {
- if ( $item->menu_item_parent && $item->menu_item_parent > 0 ) {
- $parents[] = $item->menu_item_parent;
- }
- }
-
- foreach ( $items as $item ) {
- if ( in_array( $item->ID, $parents ) ) {
- $item->classes[] = 'has-children';
- }
- }
-
- return $items;
-}
-add_filter( 'wp_nav_menu_objects', 'oenology_add_menu_parent_class' );
